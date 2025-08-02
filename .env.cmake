8b37bbd3ca8969c2c1549ac21817c9be5b425417
simone <215915761+s1mone-10@users.noreply.github.com>
2025-08-02 21:49:38 +0200

    start dev environment configuration

---
+++ .env.cmake
@@ -0,0 +1,9 @@
+set(GLFW_PATH C:/Libraries/glfw-3.4.bin.WIN64)
+set(GLM_PATH C:/Libraries/glm)
+set(VULKAN_SDK_PATH C:/Libraries/VulkanSDK/1.4.321.1)
+
+# Set MINGW_PATH if using mingwBuild.bat and not VisualStudio20XX
+set(MINGW_PATH "C:/Program Files/mingw64")
+
+# Optional set TINYOBJ_PATH to target specific version, otherwise defaults to external/tinyobjloader
+# set(TINYOBJ_PATH X:/dev/Libraries/tinyobjloader)
