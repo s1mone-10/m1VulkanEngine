518dfc41cd1329900edfcb9dca424d98bada60dd
simone <215915761+s1mone-10@users.noreply.github.com>
2025-08-19 19:22:45 +0200

    Fixed verts coordinate to avoid back face culling

--- compileShaders.bat
+++ compileShaders.bat
@@ -1,3 +1,7 @@
+@echo off
+if not exist shaders\compiled mkdir shaders\compiled
+
 C:\Libraries\VulkanSDK\1.4.321.1\Bin\glslc.exe shaders\simple.vert -o shaders\compiled\simple_vert.spv
 C:\Libraries\VulkanSDK\1.4.321.1\Bin\glslc.exe shaders\simple.frag -o shaders\compiled\simple_frag.spv
+
 pause
